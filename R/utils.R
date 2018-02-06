a06a6b5c502fc0d125587dad57bfeeab015020ba
Joe Thorley <joe@poissonconsulting.ca>
2018-02-06 15:20:35 -0800

    added internal error function

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,5 @@
 parameters_arg2to1 <- function(param_type, x, scalar_only) {
   parameters(x = x, param_type = param_type, scalar_only = scalar_only)
 }
+
+error <- function(...) stop(..., call. = FALSE)
