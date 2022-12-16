3b24c62d203174adc9b9b36fc01a063978494eb5
Joe Thorley <joe@poissonconsulting.ca>
2022-12-16 15:07:32 -0800

    sort out dependencies

--- R/namespace.R
+++ R/namespace.R
@@ -7,4 +7,5 @@
 #' @importFrom rstan extract stanc stan_model sampling
 #' @importFrom stats sd coef logLik predict update var
 #' @importFrom stringr str_c str_detect str_extract str_extract_all str_locate_all str_replace str_replace_all str_split str_sub str_trim
+#' @importFrom tibble tibble
 NULL
