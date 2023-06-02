628f9f54a3d7a27824dcb2525c0ad4c3a44d9711
Nicole Hill <nicole@poissonconsulting.ca>
2023-06-02 12:16:01 -0700

    Test exponential sensitivity

--- tests/testthat/test-code.R
+++ tests/testthat/test-code.R
@@ -19,6 +19,7 @@ test_that("code", {
       real<lower=0> tau_y; // modeled param
       vector<lower=0>[N] foo; // just for testing purposes
       real<lower=0> foo2[N,N]; // just for testing purposes
+      real<lower=-> sigma_pi;
     }
     transformed parameters {
       real<lower=0> sigma_y; // derived quantity (param)
@@ -26,6 +27,7 @@ test_that("code", {
       vector<lower=0>[N] bar; // just for testing purposes
     }
     model {
+      sigma_pi ~ exponential(10)
       sigma_mu ~ normal(0, 2);
       sigma_y ~ normal(1.1, theta);
       sigma_y ~ lognormal(alpha, 2.20);
@@ -45,27 +47,27 @@ test_that("code", {
   expect_identical(class(code), c("smb_code", "mb_code"))
   expect_true(is.smb_code(code))
 
-  expect_identical(pars(code), c("bar", "foo", "foo2", "mu_y", "sigma_y", "tau_y"))
+  expect_identical(pars(code), c("bar", "foo", "foo2", "mu_y", "sigma_pi", "sigma_y", "tau_y"))
 
   expect_identical(pars(code, "primary"),
-                   c("foo", "foo2", "mu_y", "tau_y"))
+                   c("foo", "foo2", "mu_y", "sigma_pi", "tau_y"))
 
-  expect_identical(pars(code, "primary", scalar = TRUE), c("mu_y", "tau_y"))
+  expect_identical(pars(code, "primary", scalar = TRUE), c("mu_y", "sigma_pi", "tau_y"))
 
   expect_identical(pars(code, param_type = "derived"),
                    c("bar", "sigma_y"))
   expect_identical(pars(code, param_type = "derived", scalar = TRUE),
                    c("sigma_y"))
 
-  code10 <- sd_priors_by(code, 10, distributions = c("logistic", "normal", "lognormal", "t"))
+  code10 <- sd_priors_by(code, 10, distributions = c("logistic", "normal", "lognormal", "t", "exponential"))
   expect_true(is.smb_code(code10))
   expect_identical(pars(code10), pars(code))
   expect_identical(as.character(code10),
-                   "\n    data {\n      int<lower=0> N;\n      real y[N];\n      real mu_mu;\n      real<lower=0> sigma_mu;\n    }\n    transformed data {\n      real<lower=0> alpha;\n      real<lower=0> beta;\n      alpha = 0.1;\n      beta = 0.1;\n    }\n    parameters {\n      real mu_y;\n      real<lower=0> tau_y;\n      vector<lower=0>[N] foo;\n      real<lower=0> foo2[N,N];\n    }\n    transformed parameters {\n      real<lower=0> sigma_y;\n      sigma_y = pow(tau_y, -0.5);\n      vector<lower=0>[N] bar;\n    }\n    model {\n      sigma_mu ~ normal(0, 2 * 10);\n      sigma_y ~ normal(1.1, theta);\n      sigma_y ~ lognormal(alpha, 2.20 * 10);\n      tau_y ~ gamma(alpha, beta);\n      mu_y ~ normal(mu_mu, sigma_mu);\n      for (n in 1:N) {\n        y[n] ~ normal(mu_y, sigma_y);\n      }\n    }\n    generated quantities {\n      real variance_y;\n      variance_y = sigma_y * sigma_y;\n    }")
+                   "\n    data {\n      int<lower=0> N;\n      real y[N];\n      real mu_mu;\n      real<lower=0> sigma_mu;\n    }\n    transformed data {\n      real<lower=0> alpha;\n      real<lower=0> beta;\n      alpha = 0.1;\n      beta = 0.1;\n    }\n    parameters {\n      real mu_y;\n      real<lower=0> tau_y;\n      vector<lower=0>[N] foo;\n      real<lower=0> foo2[N,N];\n      real<lower=-> sigma_pi;\n    }\n    transformed parameters {\n      real<lower=0> sigma_y;\n      sigma_y = pow(tau_y, -0.5);\n      vector<lower=0>[N] bar;\n    }\n    model {\n      sigma_pi ~ exponential(10 * 0.1)\n      sigma_mu ~ normal(0, 2 * 10);\n      sigma_y ~ normal(1.1, theta);\n      sigma_y ~ lognormal(alpha, 2.20 * 10);\n      tau_y ~ gamma(alpha, beta);\n      mu_y ~ normal(mu_mu, sigma_mu);\n      for (n in 1:N) {\n        y[n] ~ normal(mu_y, sigma_y);\n      }\n    }\n    generated quantities {\n      real variance_y;\n      variance_y = sigma_y * sigma_y;\n    }")
 
   coder <- sd_priors_by(code, 2, distributions = "normal")
   expect_true(is.smb_code(coder))
   expect_identical(pars(coder), pars(code))
   expect_identical(as.character(coder),
-                   "\n    data {\n      int<lower=0> N;\n      real y[N];\n      real mu_mu;\n      real<lower=0> sigma_mu;\n    }\n    transformed data {\n      real<lower=0> alpha;\n      real<lower=0> beta;\n      alpha = 0.1;\n      beta = 0.1;\n    }\n    parameters {\n      real mu_y;\n      real<lower=0> tau_y;\n      vector<lower=0>[N] foo;\n      real<lower=0> foo2[N,N];\n    }\n    transformed parameters {\n      real<lower=0> sigma_y;\n      sigma_y = pow(tau_y, -0.5);\n      vector<lower=0>[N] bar;\n    }\n    model {\n      sigma_mu ~ normal(0, 2 * 2);\n      sigma_y ~ normal(1.1, theta);\n      sigma_y ~ lognormal(alpha, 2.20);\n      tau_y ~ gamma(alpha, beta);\n      mu_y ~ normal(mu_mu, sigma_mu);\n      for (n in 1:N) {\n        y[n] ~ normal(mu_y, sigma_y);\n      }\n    }\n    generated quantities {\n      real variance_y;\n      variance_y = sigma_y * sigma_y;\n    }")
+                   "\n    data {\n      int<lower=0> N;\n      real y[N];\n      real mu_mu;\n      real<lower=0> sigma_mu;\n    }\n    transformed data {\n      real<lower=0> alpha;\n      real<lower=0> beta;\n      alpha = 0.1;\n      beta = 0.1;\n    }\n    parameters {\n      real mu_y;\n      real<lower=0> tau_y;\n      vector<lower=0>[N] foo;\n      real<lower=0> foo2[N,N];\n      real<lower=-> sigma_pi;\n    }\n    transformed parameters {\n      real<lower=0> sigma_y;\n      sigma_y = pow(tau_y, -0.5);\n      vector<lower=0>[N] bar;\n    }\n    model {\n      sigma_pi ~ exponential(10)\n      sigma_mu ~ normal(0, 2 * 2);\n      sigma_y ~ normal(1.1, theta);\n      sigma_y ~ lognormal(alpha, 2.20);\n      tau_y ~ gamma(alpha, beta);\n      mu_y ~ normal(mu_mu, sigma_mu);\n      for (n in 1:N) {\n        y[n] ~ normal(mu_y, sigma_y);\n      }\n    }\n    generated quantities {\n      real variance_y;\n      variance_y = sigma_y * sigma_y;\n    }")
 })
