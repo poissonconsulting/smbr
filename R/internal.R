53433d10d38fd59ef12b11515137a541cf07a479
Joe Thorley <joe@poissonconsulting.ca>
2026-04-09 08:34:31 -0700

    Recognize `cholesky_factor_corr` as a parameter type Fixes #43

---
+++ R/internal.R
@@ -0,0 +1,16 @@
+types <- function() {
+  c(
+    "int",
+    "real",
+    "vector",
+    "simplex",
+    "ordered",
+    "row_vector",
+    "matrix",
+    "corr_matrix",
+    "cov_matrix",
+    "positive_ordered",
+    "array",
+    "cholesky_factor_corr"
+  )
+}
